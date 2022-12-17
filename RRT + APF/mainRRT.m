3ad4a3836c1761c750ad4e25bf0ea7613b7229e8
SY_Huang <115967171+Huang0035@users.noreply.github.com>
2022-12-17 10:04:38 +0800

    Add files via upload

---
+++ RRT + APF/mainRRT.m
@@ -0,0 +1,109 @@
+clc;clear;close all;
+% Experimental result parameters
+% Ll = Path length; Tt = Time; Node = Number of nodes¨B
+% All_Iterations = Iterations
+Ll = []; Tt = []; Node = []; Cut_L=[]; All_Iterations = []; 
+for j=1:1:2      % Number of runs
+% Setting parameters
+p=0.2;                  % Target bias probability
+start_pose =[0,0];      % Starting point
+global goal_pose;
+goal_pose = [999,999];  % Target point
+global step;
+step = 20;              
+global kp;              % Gravitational gain
+kp=1;
+global p0;              % Range of repulsion
+p0 = 250;
+global krep;            % Repulsion gain coefficient
+krep = 2;
+bias = 1.5*step;        % Allowable error
+numNodes = 5000;        
+counter = 0;
+global x_max;global y_max;
+x_max = 1000;
+y_max = 1000;
+
+% Obstacle
+global obstacle1;global obstacle2;global obstacle3;
+global obstacle11;global obstacle22;global obstacle33;
+obstacle1 = [550,150,200,200];
+obstacle2 = [600,550,200,200];
+obstacle3 = [200,200,200,300];
+global path_V;
+path_V= ([start_pose,-1]);       
+path_E = [];                    % Branches of a random tree
+path=[];                        % Final path
+
+
+% Build map
+figure(j)
+axis([0 x_max 0 y_max])
+rectangle('Position',obstacle1,'FaceColor',[0 .5 .5])
+rectangle('Position',obstacle2,'FaceColor',[0 .5 .5])
+rectangle('Position',obstacle3,'FaceColor',[0 .5 .5])
+or = 10;                        % Obstacle influence range
+obstacle11 = [obstacle1(1)-or,obstacle1(2)-or,obstacle1(3)+2*or,obstacle1(4)+2*or];
+obstacle22 = [obstacle2(1)-or,obstacle2(2)-or,obstacle2(3)+2*or,obstacle2(4)+2*or];
+obstacle33 = [obstacle3(1)-or,obstacle3(2)-or,obstacle3(3)+2*or,obstacle3(4)+2*or];
+hold on
+plot(start_pose,'m*','LineWidth',10);
+hold on
+plot(goal_pose(1),goal_pose(2),'o','LineWidth',10);
+hold on
+
+% Algorithm implementation
+tic
+rrts=rrt;
+for i = 1:1:numNodes
+    if rand(1)<=p
+        q_rand = goal_pose;
+    else
+        q_rand = [floor(rand(1)*x_max) floor(rand(1)*y_max)];
+    end
+    [nearest_point,I]=rrts.nearestpoint(q_rand);                % Find the nearest node of the first tree
+    new_point = rrts.APF_newpoint(nearest_point,q_rand);        % Find a new node
+    if rrts.noCollision(new_point, nearest_point, obstacle11)&&rrts.noCollision(new_point, nearest_point, obstacle22)&&rrts.noCollision(new_point, nearest_point, obstacle33)
+         line([nearest_point(1),new_point(1)],[nearest_point(2),new_point(2)],'color','b','LineWidth', 2);  % Draw the expansion process
+         counter=counter+1;M(counter)=getframe;
+         path_V = [path_V;[new_point,I]];       % Add new nodes to the random tree
+    end
+    % Determine whether to reach the target point
+    if (norm((new_point-goal_pose),2)<=bias)
+        break;
+    end      
+end
+Iterations = i;
+t=toc;              % Calculate the time consumed by a search algorithm
+
+pose1 = path_V(size(path_V,1),1:3);
+while (pose1(1,3) > 0)
+    path=[pose1(1,1:2);path];
+    pose1 = path_V(pose1(1,3),1:3);   
+end
+path = [path_V(1,1:2);path];    % Join the initial point
+% Draw the final path
+plot(path(:,1)',path(:,2)','color','r','LineWidth',1.5);
+
+% Calculate the original path length
+L=0;
+for i=1:(size(path,1)-1)
+    L=L+norm(path(i,1:2)-path(i+1,1:2),2);
+end
+Ll = [Ll,L];                 
+Tt = [Tt,t];                 
+Node = [Node,size(path,1)];  
+All_Iterations = [All_Iterations,Iterations];
+end
+disp('Average original path length');  mean(Ll)
+disp('Average experiment time');  mean(Tt)
+disp('Average number of experimental path nodes');  mean(Node)
+disp('Average number of experiment iterations');  mean(All_Iterations)
+
+
+
+
+
+
+
+
---
+++ RRT + APF/rrt.m
@@ -0,0 +1,154 @@
+function rrts = rrt
+rrts.isEdgeCollisionFree= @isEdgeCollisionFree;           % Determine whether there is an obstacle in the line between two points
+rrts.isOutOfBounds = @isOutOfBounds;                      % Judging whether the point exceeds the obstacle
+rrts.euclidian_dist = @euclidian_dist;                    % Calculate the distance between two points
+rrts.arangel= @ arangel;                                  % Calculates the angle between two vectors
+rrts.direction = @direction;                              % Compute a two-point vector
+rrts.samplesobol = @samplesobol;                          % Sample Sobol sequence
+rrts.nearestpoint = @nearestpoint;                        % Calculate nearest node
+rrts.nearestpoint2 = @nearestpoint2;                      % Calculate the closest node of the second tree
+rrts.angel_dist = @angel_dist;                            % Calculate the distance between joint angles
+rrts.newpoint = @newpoint;                                % Generate new nodes
+rrts.findparentpoint = @findparentpoint;                  % Find the parent node of the current node
+rrts.ccw=@ccw;
+rrts.noCollision=@noCollision;
+rrts.APF_newpoint=@APF_newpoint;
+rrts.REP_newpoint=@REP_F;
+end
+
+function val = ccw(A,B,C)
+    val = (C(2)-A(2)) * (B(1)-A(1)) > (B(2)-A(2)) * (C(1)-A(1));
+end
+
+function nc = noCollision(n2, n1, o)
+    A = [n1(1) n1(2)];
+    B = [n2(1) n2(2)];
+    obs = [o(1) o(2) o(1)+o(3) o(2)+o(4)];   
+    C1 = [obs(1),obs(2)];
+    D1 = [obs(1),obs(4)];
+    C2 = [obs(1),obs(2)];
+    D2 = [obs(3),obs(2)];
+    C3 = [obs(3),obs(4)];
+    D3 = [obs(3),obs(2)];
+    C4 = [obs(3),obs(4)];
+    D4 = [obs(1),obs(4)];
+    ints1 = ccw(A,C1,D1) ~= ccw(B,C1,D1) && ccw(A,B,C1) ~= ccw(A,B,D1); 
+    ints2 = ccw(A,C2,D2) ~= ccw(B,C2,D2) && ccw(A,B,C2) ~= ccw(A,B,D2);
+    ints3 = ccw(A,C3,D3) ~= ccw(B,C3,D3) && ccw(A,B,C3) ~= ccw(A,B,D3);
+    ints4 = ccw(A,C4,D4) ~= ccw(B,C4,D4) && ccw(A,B,C4) ~= ccw(A,B,D4);
+    if ints1==0 && ints2==0 && ints3==0 && ints4==0
+        nc = 1;
+    else
+        nc = 0;
+    end
+end
+
+% Compute the vector determined by two points
+function [dir] = direction(pose1,pose2)
+    dir1 = pose2(1)-pose1(1);
+    dir2 = pose2(2)-pose1(2);
+    dir = [dir1 dir2];   
+end
+
+% Calculate the angle between two phases
+function Flag = angel_limit(path,xnear,xnew)
+    global angle;
+    if xnear(3)<0
+        Flag = 1;
+    else
+        xinit = path(xnear(3),1:3);
+        A = [xinit(1)-xnear(1),xinit(2)-xnear(2)];
+        B = [xnew(1)-xnear(1),xnew(2)-xnear(2)];
+        thea = acos(dot(A,B)/(norm(A)*norm(B)))*180/pi;
+        if thea>=angle
+            Flag = 1;
+        else
+            Flag = 0; 
+        end
+    end
+end
+
+% Find the distance between two points
+function [angel_len] = angel_dist(pose1,pose2)
+    angel_len = norm((pose2-pose1),2);
+end
+
+% Calculate nearest node
+function [thea_dis,I]= nearestpoint(random_point)
+    global path_V;
+    min_dis = inf;
+    for i = 1: size(path_V,1)
+        if(min_dis>angel_dist(random_point,path_V(i,1:2)))
+            min_dis = angel_dist(random_point,path_V(i,1:2));
+            thea_dis = [path_V(i,1:3)];
+            I = i;
+        end        
+    
+    end
+end
+
+% Find a new node according to the step size
+function [new_thea] = newpoint(near_point,random_point)
+    global step;
+    len = norm((random_point - near_point(1:2)),2);
+    if(len < step)
+        new_thea = random_point;
+    else
+        dir = (random_point - near_point(1:2))/len;
+        new_thea = near_point(1:2) + dir*step;
+    end
+end
+
+% Repulsion function
+function [F] = REP_F(near_point,random_point,obstacle)
+    global goal_pose;
+    global p0;                                                  % Range of repulsion  
+    px = norm([near_point(1)-(obstacle(1)+obstacle(3)/2),near_point(2)-(obstacle(2)+obstacle(4)/2)],2);     % The distance from the nearest node to the obstacle
+    if px>p0
+        F=[0,0];
+    else
+        F = (1/px-1/p0)/(px)^2*([near_point(1)-(obstacle(1)+obstacle(3)/2),near_point(2)-(obstacle(2)+obstacle(4)/2)])/px;
+    end
+end
+
+% Node generating function with gravitational and repulsive fields added
+function [new_thea] = APF_newpoint(near_point,random_point)
+    global obstacle11;global obstacle22;global obstacle33;
+    global step;
+    global goal_pose;
+    global kp;
+    global krep;                                            % repulsion gain coefficient
+    global tem_step;
+    len = norm((random_point - near_point(1:2)),2);         % The distance between the sampling point and the nearest node
+    len1 = norm((goal_pose - near_point(1:2)),2);           % The distance between the target point and the nearest node
+    F1 = REP_F(near_point,random_point,obstacle11);
+    F2 = REP_F(near_point,random_point,obstacle22);
+    F3 = REP_F(near_point,random_point,obstacle33);
+    if (F1 + F2 + F3) == 0
+        F = [0,0];
+    else
+        F = krep*(F1 + F2 + F3)/norm((F1 + F2 + F3),2);
+    end    
+    if(len < step)
+        tem_step = len;
+    else
+        tem_step = step;
+    end
+    U = (random_point - near_point(1:2))/len+kp*(goal_pose - near_point(1:2))/len1+F;
+    new_thea = near_point(1:2)+tem_step*U/norm(U,2);
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
